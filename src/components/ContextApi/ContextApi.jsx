71a4e6fb09109d640b61e13f73df09da917916c1
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-08 14:53:03 +0600

    initial setup donr

---
+++ src/components/ContextApi/ContextApi.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContextApi = () => {
+    return (
+        <div>
+            <h2>THis is contextApi</h2>
+        </div>
+    );
+};
+
+export default ContextApi;
